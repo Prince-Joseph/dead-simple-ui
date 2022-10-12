067b53a671c4b78e53a84aeb21a845ec522a2707
S E Prince Joseph <sindala.prince@gmail.com>
2022-10-12 19:47:26 +0530

    + gulp workflow setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,52 +2,63 @@ var gulp = require("gulp");
 const nunjucksRender = require("gulp-nunjucks-render");
 const purgecss = require("gulp-purgecss");
 const uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-
+var sass = require('gulp-sass')(require("sass"));
+var rename = require("gulp-rename");
 const browserSync = require("browser-sync").create();
 
 function nunjucks_to_html() {
   return (
     gulp
-      .src("src/pages/**/*.+(html|nunjucks|njk)")
+      .src("src/**/*.+(html|nunjucks|njk)")
       .pipe(
         nunjucksRender({
-          path: ["src/templates"],
+          path: ["src/components"],
         })
       )
       .pipe(gulp.dest("build"))
   );
 }
 
-gulp.task("nunjucks", nunjucks_to_html);
+
+function compileSass(){
+  return gulp.src('./src/assets/sass/**/*.scss') 
+  .pipe(sass().on('error', sass.logError)) 
+  .pipe(gulp.dest('./src/assets/css/')); 
+}
+
 
 function buildStyle() {
   return gulp
-    .src("src/assets/css/**/*.css")
-    .pipe(purgecss({ content: ["build/*.html", "build/assets/js/**/*.js"] }))
-    .pipe(gulp.dest("build/assets/css"));
+  .src("src/assets/css/**/*.css")
+  .pipe(purgecss({ content: ["build/*.html", "build/assets/js/**/*.js"] }))
+  .pipe(rename(function (path) {
+    path.basename += ".min";
+    path.extname = ".css";
+  }))
+  .pipe(gulp.dest("build/assets/css"));
 }
-gulp.task("css", buildStyle);
 
 function uglifyJs(){
-    return gulp.src(['src/assets/js/*.js'])
-    .pipe(uglify())
-    .pipe(gulp.dest("build/assets/js"));
+  return gulp.src(['src/assets/js/*.+(js|ts)'])
+  .pipe(uglify())
+  .pipe(rename(function (path) {
+    path.basename += ".min";
+    path.extname = ".js";
+  }))
+  .pipe(gulp.dest("build/assets/js"));
 };
-gulp.task("js", uglifyJs);
 
-gulp.task("watch", function () {
-    
-  browserSync.init({
-    server: {
-      baseDir: "build",
-    },
-  });
-  
-  gulp
-    .watch(["./build/**/*.html", "./build/**/*.css", "./build/**/*.js"])
-    .on("change", browserSync.reload);
+function watchFiles() {
+  browserSync.init({server:{baseDir: "build", },});
+  gulp.watch(["./build/**/*.html", "./build/**/*.css", "./build/**/*.js"]).on("change", browserSync.reload);
   gulp.watch("./src/**/**/*.njk", gulp.series("nunjucks"));
+  gulp.watch("./src/assets/sass/**/*.scss", gulp.series("sass"));
   gulp.watch("./src/assets/css/**/*.css", gulp.series("css"));
   gulp.watch("./src/assets/js/**/*.js", gulp.series("js",));
-});
+}
+
+gulp.task("nunjucks", nunjucks_to_html);
+gulp.task("sass", compileSass);
+gulp.task("css", buildStyle);
+gulp.task("js", uglifyJs);
+gulp.task("watch", watchFiles);
